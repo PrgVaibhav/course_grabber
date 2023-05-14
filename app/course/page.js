5d49ea7eb4362802821ee77f6e8b6ae2a199203e
prgVaibhav <vaibhav2k1507@gmail.com>
2023-05-14 12:46:17 +0530

    Initial Commits

---
+++ app/course/page.js
@@ -0,0 +1,7 @@
+export default function course() {
+  return (
+    <div>
+      <h1>Course Grabber</h1>
+    </div>
+  );
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1>Navbar</h1>
+      <Link href="/course"> course </Link>
+    </div>
+  );
+}
