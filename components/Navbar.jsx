ca6c53cb685f8ce5c39f790026e623735cc9b51f
scragonrex <scragondragon@gmail.com>
2023-05-14 22:28:10 +0530

    navbar update1

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,18 @@
+'use client'
 import Link from "next/link";
-
+import styles from '../app/navbar.module.css';
+import { useRouter } from "next/navigation";
 export default function Navbar() {
+  const router = useRouter();
   return (
-    <div>
-      <h1>Navbar</h1>
-      <Link href="/course"> course </Link>
-      <Link href="/post"> post </Link>
+    <div className={styles.navbar}>
+      <div className={styles.logo}>Coursegrabber</div>
+      <div className={styles.links}>
+      {/* <Link className={router.pathname==='/'? styles.activeLink : styles.link} href="/">Home</Link> */}
+      <Link className={styles.link} href="/">Home</Link>
+      {/* <Link className={router.pathname==='/course'? styles.activeLink : styles.link} href="/course">Course</Link> */}
+      <Link className={styles.link} href="/course">Course</Link>
+      </div>
     </div>
   );
 }
