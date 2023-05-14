83ecc639ec7f9f512832258955e2df715e58a634
prgVaibhav <vaibhav2k1507@gmail.com>
2023-05-14 23:23:03 +0530

    Initial commits

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,19 @@
-'use client'
+"use client";
 import Link from "next/link";
-import styles from '../styles/navbar.module.css';
+import styles from "../styles/navbar.module.css";
 import { useRouter } from "next/navigation";
 export default function Navbar() {
   const router = useRouter();
   return (
     <div className={styles.navbar}>
-      <div className={styles.logo}>Coursegrabber</div>
+      <div className={styles.logo}>Course Grabber</div>
       <div className={styles.links}>
-      <Link className={styles.link} href="/">Home</Link>
-      <Link className={styles.link} href="/course">Course</Link>
+        <Link className={styles.link} href="/">
+          Home
+        </Link>
+        <Link className={styles.link} href="/course">
+          Course
+        </Link>
       </div>
     </div>
   );
