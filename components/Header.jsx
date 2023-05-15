a1ec8b1a2c38177d120c1d4c6b1fecfd33752d20
Varghab <shibvarghab@gmail.com>
2023-05-15 06:24:31 +0530

    added_header_image

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,12 +4,11 @@ import styles from "../styles/header.module.css";
 export default function Header() {
   return (
     <section className={styles.header_container}>
-      {/* <div className={styles.header_wrapper}> */}
+      <div className={styles.header_wrapper}>
       <div className={styles.header_title}>
         <span className={styles.first_span}>Course</span>{" "}
         <span className={styles.second_span}>Grabber</span>
-      </div>
-      <div className={styles.header_description}>
+        <div className={styles.header_description}>
         <p>
           Course Grabber is a one-stop platform that provides free online
           courses from popular video sharing platforms such as YouTube and other
@@ -21,10 +20,13 @@ export default function Header() {
           student, professional, or simply someone who enjoys learning new
           things, Course Grabber has something for everyone.
         </p>
-
         <Link href="/course">Start Learning</Link>
       </div>
-      {/* </div> */}
+      </div>
+      <div>
+        <img src='/4320858.jpg' className={styles.headerImage}></img>
+      </div>
+      </div>
     </section>
   );
 }
